6a6afd6b61001a8e077e8324dcbb29442e4e3f8e
gchal <chalvatzaki@ias.informatik.tu-darmstadt.de>
2020-11-19 16:22:17 +0100

    fix: support for older yaml versions

--- gibson2/__init__.py
+++ gibson2/__init__.py
@@ -7,7 +7,7 @@ __version__ = "0.0.5"
 logging.getLogger().setLevel(logging.INFO)
 
 with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'global_config.yaml')) as f:
-    global_config = yaml.load(f, Loader=yaml.FullLoader)
+    global_config = yaml.safe_load(f)
 
 assets_path = global_config['assets_path']
 dataset_path = global_config['dataset_path']
